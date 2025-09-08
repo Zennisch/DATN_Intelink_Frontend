c2cb7fb32a7558bd8e2e6104ddb458221eba08be
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-09-09 05:24:52 +0900

    update

--- intelink-project/src/pages/LoginPage.tsx
+++ intelink-project/src/pages/LoginPage.tsx
@@ -62,7 +62,7 @@ export function LoginPage() {
 
 	return (
 		<div className="min-h-screen bg-gray-50 font-inter">
-			<Header />
+			<Header onLanguageChange={() => {}} />
 
 			<div className="flex items-center justify-center min-h-screen pt-20">
 				<div className="w-full max-w-2xl p-8">
