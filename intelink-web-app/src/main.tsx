e2f326090cfbe4ad4c6ada22475f498489a8bade
Zennisch <zennisch@gmail.com>
2025-11-02 18:28:09 +0900

    update

--- intelink-web-app/src/main.tsx
+++ intelink-web-app/src/main.tsx
@@ -5,6 +5,9 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+localStorage.setItem("ACCESS_TOKEN", import.meta.env.VITE_TEST_ACCESS_TOKEN);
+localStorage.setItem("REFRESH_TOKEN", import.meta.env.VITE_TEST_REFRESH_TOKEN);
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
