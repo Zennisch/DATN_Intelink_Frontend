0cde0caba8cf2fd6406f72345ef483acca7c9dc8
Zennisch <zennisch@gmail.com>
2025-11-02 19:17:11 +0900

    update

--- intelink-web-app/src/main.tsx
+++ intelink-web-app/src/main.tsx
@@ -4,14 +4,20 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { ShortUrlProvider } from "./contexts/ShortUrlContext.tsx";
+import { setupAxios } from "./services/AxiosConfig.ts";
 
 localStorage.setItem("ACCESS_TOKEN", import.meta.env.VITE_TEST_ACCESS_TOKEN);
 localStorage.setItem("REFRESH_TOKEN", import.meta.env.VITE_TEST_REFRESH_TOKEN);
 
+setupAxios();
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ShortUrlProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ShortUrlProvider>
 	</StrictMode>,
 );
