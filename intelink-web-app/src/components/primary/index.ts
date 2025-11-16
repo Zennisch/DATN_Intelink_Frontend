d4ccfec149b2b2fa5b022a2b0033eea4f0c7099f
Zennisch <zennisch@gmail.com>
2025-11-16 16:09:54 +0900

    create: ui primary components

---
+++ intelink-web-app/src/components/primary/index.ts
@@ -0,0 +1,10 @@
+import { Button } from "./Button.tsx";
+import { Checkbox } from "./Checkbox.tsx";
+import { Input } from "./Input.tsx";
+import { Modal } from "./Modal.tsx";
+import { Radio } from "./Radio.tsx";
+import { Select } from "./Select.tsx";
+import { Slider } from "./Slider.tsx";
+import { Toggle } from "./Toggle.tsx";
+
+export { Button, Checkbox, Input, Modal, Radio, Select, Slider, Toggle };
