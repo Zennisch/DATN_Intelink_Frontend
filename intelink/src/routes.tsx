3a9c3aa1b5aaa0cb1970b8d0e10a2dbc43fe4156
Zennisch <zennisch@gmail.com>
2025-12-09 20:52:59 +0900

    update: apply route

---
+++ intelink/src/routes.tsx
@@ -0,0 +1,31 @@
+import {lazy, type ReactNode} from 'react';
+
+const LoginPage = lazy(() => import('./pages/LoginPage').then((m) => ({default: m.default})));
+const RegisterPage = lazy(() => import('./pages/RegisterPage').then((m) => ({default: m.default})));
+const ForgotPasswordPage = lazy(() => import('./pages/ForgotPasswordPage').then((m) => ({default: m.default})));
+const ResetPasswordPage = lazy(() => import('./pages/ResetPasswordPage').then((m) => ({default: m.default})));
+
+export interface RouteConfig {
+	path: string;
+	element: ReactNode;
+	requireAuth?: boolean;
+}
+
+export const routes: RouteConfig[] = [
+	{
+		path: '/login',
+		element: <LoginPage />,
+	},
+	{
+		path: '/register',
+		element: <RegisterPage />,
+	},
+	{
+		path: '/forgot-password',
+		element: <ForgotPasswordPage />,
+	},
+	{
+		path: '/reset-password',
+		element: <ResetPasswordPage />,
+	},
+];
