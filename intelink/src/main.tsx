6b235ada223a61d0a112ea7002794f8da0476745
Zennisch <zennisch@gmail.com>
2025-12-09 20:37:39 +0900

    update

--- intelink/src/main.tsx
+++ intelink/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode, Suspense} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import {BrowserRouter} from 'react-router-dom';
+import {AuthProvider} from './hooks/useAuth.tsx';
+import PageSpinner from './components/layout/PageSpinner.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+			<AuthProvider>
+				<Suspense fallback={<PageSpinner />}>
+					<App />
+				</Suspense>
+			</AuthProvider>
+		</BrowserRouter>
+	</StrictMode>
+);
